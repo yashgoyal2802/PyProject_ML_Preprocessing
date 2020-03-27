16ef9f89d5e33ced245f76d1c9ef8cfc012832d0
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-03-27 17:21:57 +0530

    updated sdk

--- main_gui.py
+++ main_gui.py
@@ -9,8 +9,7 @@ var3 = IntVar()
 var4 = IntVar()
 var5 = IntVar()
 
-text1 = Label(window, text='Enter the Path to all your Images')
-text1.grid(row=0)
+text1 = Label(window, text='Enter the Path to all your Images', anchor=NE).grid()
 
 text2 = Label(window, text='Enter the Path')
 text2.grid(row=1, column=0)
@@ -27,7 +26,7 @@ Label(window, text='Enter width and height').grid(row=3, column=1)
 b_width = Entry(window)
 b_width.grid(row=3, column=2)
 b_height = Entry(window)
-b_height.grid(row=3, column=4)
+b_height.grid(row=3, column=3)
 
 c = Checkbutton(window, text='Convert Images to Greyscale', variable=var3)
 c.grid(row=4, column=0)
@@ -35,15 +34,15 @@ c.grid(row=4, column=0)
 d = Checkbutton(window, text='Store Images in numpy array with extracted features')
 d.grid(row=5, column=0)
 
-Label(window, text='Extraction Parameters:').grid(row=6)
+Label(window, text='Extraction Parameters:').grid(row=6, column=0)
 
 Label(window, text="Enter the feature separating symbol (E.g:- '_' or '-'):  ").grid(row=7, column=0)
 e = Entry(window)
-e.grid(row=7, column=2)
+e.grid(row=7, column=1)
 
 Label(window, text="Enter the offset of the separating symbol (i.e. after how many '_' or '-' to start extracting the features:  ").grid(row=8, column=0)
 f = Entry(window)
-f.grid(row=8, column=2)
+f.grid(row=8, column=1)
 
 g = Text(window, height=7, width=50, state='disabled')
 g.configure(state='normal')
